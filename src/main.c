f92ccbcc6c7aeb6015d7d01f60b51b39ea55579a
xenon2 <jakub.kubica@gmail.com>
2026-08-29 16:55:40 +0200

    removed unused stuff

--- src/main.c
+++ src/main.c
@@ -11,13 +11,8 @@ NO ANY WARRANTY
 #include "utils.h"
 
 static const ViewerOps *active_viewer;
-
-typedef enum ViewerMode {
-    MODE_NONE = -2,
-    MODE_QUIT = -1,
-    VGA,
-    EGA
-} ViewerMode;
+static int vga_available;
+static int ega_available;
 
 #define KEY_INFO 'i'
 
@@ -25,7 +20,6 @@ static int viewer_available(const ViewerOps *viewer)
 {
     return viewer->image_exists(1) && viewer->hardware_available();
 }
-
 
 static void set_txt_mode(void)
 {
@@ -35,8 +29,6 @@ static void set_txt_mode(void)
     int86(0x10, &r, &r);
 }
 
-
-
 static void banner_line(const char *text)
 {
     printf("| %-39s |\n", text);
@@ -45,8 +37,6 @@ static void banner_line(const char *text)
 static void banner(void)
 {
     char build_info[40];
-    int vga_available = viewer_available(&vga_viewer_ops);
-    int ega_available = viewer_available(&ega_viewer_ops);
 
     pc_beep(140, 50);
     pc_beep(240, 50);
@@ -62,17 +52,14 @@ static void banner(void)
     banner_line(build_info);
     banner_line("Choose your display mode:");
 
-    if (vga_available) {
+    if (vga_available)
         banner_line("[1] or [<-]  VGA  320x200 / 256 colors");
-    }
 
-    if (ega_available) {
+    if (ega_available)
         banner_line("[2] or [->]  EGA  640x350 / 16 colors");
-    }
 
-    if (!vga_available && !ega_available) {
+    if (!vga_available && !ega_available)
         banner_line("No supported video mode or image files!");
-    }
 
     banner_line("");
     banner_line("[ESC] Quit");
@@ -82,8 +69,8 @@ static void banner(void)
     printf("+-----------------------------------------+\n");
 }
 
-
-static void exit_prog(void) {
+static void exit_prog(void)
+{
     pc_beep(340, 50);
     pc_beep(240, 50);
     pc_beep(140, 50);
@@ -111,7 +98,8 @@ static int next_image(int current)
     return current;
 }
 
-static void images_key_loop(void) {
+static void images_key_loop(void)
+{
     int current = 1;
     int key;
 
@@ -129,7 +117,6 @@ static void images_key_loop(void) {
             read_key();
         }
 
-
         switch (KEY_SCAN(key))
         {
             case SCAN_LEFT:
@@ -140,124 +127,53 @@ static void images_key_loop(void) {
                 current = next_image(current);
                 break;
         }
-       active_viewer->transition();
+
+        active_viewer->transition();
     }
 }
 
+static const ViewerOps *viewer_from_key(int key)
+{
+    if ((KEY_ASCII(key) == '1' ||
+         (KEY_ASCII(key) == 0 && KEY_SCAN(key) == SCAN_LEFT)) &&
+        vga_available)
+        return &vga_viewer_ops;
 
-static void images_auto_loop(void) {
-    int current = 1;
+    if ((KEY_ASCII(key) == '2' ||
+         (KEY_ASCII(key) == 0 && KEY_SCAN(key) == SCAN_RIGHT)) &&
+        ega_available)
+        return &ega_viewer_ops;
+
+    return NULL;
+}
+
+static const ViewerOps *select_viewer(void)
+{
+    const ViewerOps *viewer;
     int key;
-    int looper = 0;
-
-     for (;;) {
-        active_viewer->load_palette(current);
-        active_viewer->load_image(current);
-
-        for (looper=0;looper<10;looper++) {
-            sleep_nseconds(100);
-            key = read_key_nb();
-            if (key != -1 && KEY_ASCII(key) == KEY_ESC) {
-                looper = 200;
-            }
-        }
-        current++;
-        if (!active_viewer->image_exists(current))
-            current = 1;
-
-       active_viewer->transition();
-    }
-
-}
-
-static void set_mode(ViewerMode mode) {
-    switch (mode) {
-        case VGA:
-            active_viewer = &vga_viewer_ops;
-            break;
-
-        case EGA:
-            active_viewer = &ega_viewer_ops;
-            break;
-
-        default:
-            return;
-    }
-
-    active_viewer->set_video_mode();
-}
-
-static const char *mode_name(ViewerMode mode)
-{
-    switch (mode) {
-        case VGA:
-            return "VGA";
-
-        case EGA:
-            return "EGA";
-    }
-
-    return "unknown";
-}
-
-static ViewerMode mode_from_key(int key)
-{
-    if (KEY_ASCII(key) == KEY_ESC)
-        return MODE_QUIT;
-
-    if (KEY_ASCII(key) == '1' && viewer_available(&vga_viewer_ops))
-        return VGA;
-
-    if (KEY_ASCII(key) == '2' && viewer_available(&ega_viewer_ops))
-        return EGA;
-
-    if (KEY_ASCII(key) != 0)
-        return MODE_NONE;
-
-    switch (KEY_SCAN(key))
-    {
-        case SCAN_LEFT:
-            if (viewer_available(&vga_viewer_ops))
-                return VGA;
-            break;
-
-        case SCAN_RIGHT:
-            if (viewer_available(&ega_viewer_ops))
-                return EGA;
-            break;
-    }
-
-    return MODE_NONE;
-}
-
-static ViewerMode select_mode(void)
-{
-    ViewerMode mode;
 
     for (;;) {
-        mode = mode_from_key(read_key());
-        if (mode != MODE_NONE)
-            return mode;
+        key = read_key();
+        if (KEY_ASCII(key) == KEY_ESC)
+            return NULL;
+
+        viewer = viewer_from_key(key);
+        if (viewer)
+            return viewer;
     }
-}
-
-static void start_viewer(ViewerMode mode)
-{
-    printf("%s mode selected, switching...\n", mode_name(mode));
-    set_mode(mode);
-
-    images_key_loop();
 }
 
 int main(void)
 {
-    ViewerMode mode;
-
+    vga_available = viewer_available(&vga_viewer_ops);
+    ega_available = viewer_available(&ega_viewer_ops);
     banner();
 
-    mode = select_mode();
-    if (mode != MODE_QUIT)
-        start_viewer(mode);
+    active_viewer = select_viewer();
+    if (active_viewer) {
+        active_viewer->set_video_mode();
+        images_key_loop();
+    }
 
     set_txt_mode();
     exit_prog();
